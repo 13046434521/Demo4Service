3f2dc2ce1a590d478013df5f2250ffad75f78c01
贾天龙 <604872657@qq.com>
2019-09-19 20:52:44 +0800

    by jtl

--- demo/src/main/java/com/jtl/demo4service/DemoService.java
+++ demo/src/main/java/com/jtl/demo4service/DemoService.java
@@ -2,7 +2,11 @@ package com.jtl.demo4service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.widget.Toast;
+
+import com.socks.library.KLog;
 
 import androidx.annotation.Nullable;
 
@@ -14,34 +18,47 @@ import androidx.annotation.Nullable;
  */
 public class DemoService extends Service {
     private static final String TAG=DemoService.class.getSimpleName();
+    private MyBinder mMyBinder=new MyBinder();
     @Override
     public void onCreate() {
         super.onCreate();
+        KLog.d(TAG,"onCreate");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        KLog.d(TAG,"onStartCommand");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        KLog.d(TAG,"onBind");
+        return mMyBinder;
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
+        KLog.d(TAG,"onUnbind");
         return super.onUnbind(intent);
     }
 
     @Override
     public void onRebind(Intent intent) {
         super.onRebind(intent);
+        KLog.d(TAG,"onRebind");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        KLog.d(TAG,"onDestroy");
+    }
+
+    public class MyBinder extends Binder{
+        public void showToastByService(String msg){
+            Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- demo/src/main/java/com/jtl/demo4service/MainActivity.java
+++ demo/src/main/java/com/jtl/demo4service/MainActivity.java
@@ -2,13 +2,90 @@ package com.jtl.demo4service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.socks.library.KLog;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mStartBtn;
+    private Button mStopBtn;
+    private Button mBindBtn;
+    private Button mUnBindBtn;
+    private EditText mDataEdit;
+    private Button mTestBtn;
+    private Connection mConnection;
+
+    private DemoService.MyBinder mMyBinder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mConnection=new Connection();
+
+        mStartBtn=findViewById(R.id.btn_service_start);
+        mStopBtn=findViewById(R.id.btn_service_stop);
+        mBindBtn=findViewById(R.id.btn_service_bind);
+        mUnBindBtn=findViewById(R.id.btn_service_unbind);
+        mTestBtn=findViewById(R.id.btn_service_test);
+        mDataEdit=findViewById(R.id.et_service_data);
+
+        addOnClickListener(mStartBtn,mStopBtn,mBindBtn,mUnBindBtn,mTestBtn);
+    }
+
+    private void addOnClickListener(View ... views) {
+        for (View view:views){
+            view.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_service_start:
+                Intent startIntent=new Intent(this,DemoService.class);
+                startService(startIntent);
+                break;
+            case R.id.btn_service_stop:
+                Intent stopIntent=new Intent(this,DemoService.class);
+                stopService(stopIntent);
+                break;
+
+            case R.id.btn_service_bind:
+                Intent bindIntent=new Intent(this,DemoService.class);
+                bindService(bindIntent,mConnection,BIND_AUTO_CREATE);
+                break;
+            case R.id.btn_service_unbind:
+                unbindService(mConnection);
+                break;
+            case R.id.btn_service_test:
+                if (!TextUtils.isEmpty(mDataEdit.getText())&&mMyBinder!=null){
+                    mMyBinder.showToastByService(mDataEdit.getText().toString());
+                }
+                break;
+        }
+    }
+
+    private class Connection implements ServiceConnection{
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            KLog.d(name.toString());
+            //注意这里不能跨进程，否则转换会报错
+            mMyBinder= (DemoService.MyBinder) service;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            KLog.d("onServiceDisconnected"+name.toString());
+        }
     }
 }
